814041c09081479346ff82961fd03170f92b2cc6
Subrata Sikdar <103638344+subrata055@users.noreply.github.com>
2026-08-16 11:13:11 +0530

    Create security groups for Jenkins and SonarQube
    Added security groups for Jenkins and SonarQube with appropriate ingress and egress rules.

---
+++ terraform-devops-infra/security_groups.tf
@@ -0,0 +1,65 @@
+resource "aws_security_group" "jenkins_sg" {
+  name        = "${var.project_name}-jenkins-sg"
+  description = "Security group for Jenkins server"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins UI"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-jenkins-sg"
+  }
+}
+
+resource "aws_security_group" "sonarqube_sg" {
+  name        = "${var.project_name}-sonarqube-sg"
+  description = "Security group for SonarQube server"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SonarQube UI"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sonarqube-sg"
+  }
+}
