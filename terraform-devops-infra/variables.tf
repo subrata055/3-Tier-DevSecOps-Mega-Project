0b476e1a648efdf84af22d30a68a32366f3c6b94
Subrata Sikdar <103638344+subrata055@users.noreply.github.com>
2026-08-16 11:10:38 +0530

    Add variables for AWS infrastructure configuration

---
+++ terraform-devops-infra/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  type    = string
+  default = "devsecops-prod"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.10.0/24", "10.0.20.0/24"]
+}
+
+variable "key_name" {
+  type    = string
+  default = ""
+}
